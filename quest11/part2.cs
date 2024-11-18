24cd01e9fbca3c04745cf35faa2817a54f8a21fa
Filip Tokarski <filip@tokarski.biz>
2024-11-19 00:47:01 +0100

    2024 quest 11

---
+++ quest11/part2.cs
@@ -0,0 +1,40 @@
+﻿var line = Console.ReadLine();
+var lines = new List<string>();
+
+while (line != null || line == "")
+{
+    lines.Add(line);
+    line = Console.ReadLine();
+}
+
+var dict = new Dictionary<char, List<char>>();
+
+foreach (var l in lines)
+{
+    var ll = l.Split(':').ToList();
+    var key = ll[0][0];
+    var value = ll[1].Split(',').Select(x => x[0]).ToList();
+
+    dict.Add(key, value);
+}
+
+var days = 10;
+
+var termites = new List<char>() { 'Z' };
+
+for (int i = 0; i < days; i++)
+{
+    var newTermites = new List<char>();
+
+    foreach (var t in termites)
+    {
+        if (dict.ContainsKey(t))
+        {
+            newTermites.AddRange(dict[t]);
+        }
+    }
+
+    termites = newTermites;
+}
+
+Console.WriteLine(termites.Count);
---
+++ quest11/part3.cs
@@ -0,0 +1,61 @@
+﻿var line = Console.ReadLine();
+var lines = new List<string>();
+
+while (line != null || line == "")
+{
+    lines.Add(line);
+    line = Console.ReadLine();
+}
+
+var dict = new Dictionary<string, List<string>>();
+
+foreach (var l in lines)
+{
+    var ll = l.Split(':').ToList();
+    var key = ll[0];
+    var value = ll[1].Split(',').ToList();
+
+    dict.Add(key, value);
+}
+
+var days = 20;
+
+var keys = dict.Keys.ToList();
+
+var min = long.MaxValue;
+var max = 0L;
+
+foreach (var k in keys)
+{
+    var termites = new Dictionary<string, long> { { k, 1 } };
+
+    for (long i = 0; i < days; i++)
+    {
+        var newTermites = new Dictionary<string, long> { };
+
+        foreach (var (k2, v) in termites)
+        {
+            if (dict.ContainsKey(k2))
+            {
+                foreach (var t in dict[k2])
+                {
+                    if (newTermites.ContainsKey(t))
+                    {
+                        newTermites[t] += v;
+                    }
+                    else
+                    {
+                        newTermites.Add(t, v);
+                    }
+                }
+            }
+        }
+
+        termites = newTermites;
+    }
+
+    min = Math.Min(min, termites.Sum(t => t.Value));
+    max = Math.Max(max, termites.Sum(t => t.Value));
+}
+
+Console.WriteLine(max - min);
